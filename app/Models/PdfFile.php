75f6f9e9a0931f6b320dd6e309874f865af61d4a
aamal <aamal.darkal@gmail.com>
2023-07-24 09:48:10 +0300

    fix

--- app/Models/PdfFile.php
+++ app/Models/PdfFile.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pdfFile extends Model
+class PdfFile extends Model
 {
     use HasFactory;
 
