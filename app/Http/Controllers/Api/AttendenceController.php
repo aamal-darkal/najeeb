fab336c4bd94ed9e3197317b403f684c20786ead
aamal <aamal.darkal@gmail.com>
2023-08-01 23:41:29 +0300

    attendences - f

--- app/Http/Controllers/Api/AttendenceController.php
+++ app/Http/Controllers/Api/AttendenceController.php
@@ -12,31 +12,42 @@ class AttendenceController extends Controller
 {
     public function setAttendence($lecture_id)
     {
-        $student = Student::where('user_id',Auth::id())->first();
-        $lecture = $student->lectures()->wherePivot('lecture_id' , $lecture_id)->first();
-        
-        if ($lecture)
-        {   
-            $lecture->pivot->update(['views' => $lecture->pivot->views + 1])    ;
+        $student = Student::where('user_id', Auth::id())->first();
+        $lecture = $student->lectures()->wherePivot('lecture_id', $lecture_id)->first();
+
+        if ($lecture) {
+            $lecture->pivot->update(['views' => $lecture->pivot->views + 1]);
             $lecture->save();
-        }
-        else {
-            $student->lectures()->attach($lecture_id,['date' => Carbon::now() , 'views' =>1]);
-            $lecture = $student->lectures()->wherePivot('lecture_id' , $lecture_id)->first();
+        } else {
+            $student->lectures()->attach($lecture_id, ['date' => Carbon::now(), 'views' => 1]);
+            $lecture = $student->lectures()->wherePivot('lecture_id', $lecture_id)->first();
         }
 
         // return ResponseHelper::success(['views' => $lecture->views], 'lecture has been viewed');
         return ResponseHelper::success(['views' => $lecture->pivot->views], 'lecture has been viewed');
-    }   
+    }
 
     public function getTodayAttendences()
     {
-        $student = Student::where('user_id',Auth::id())->first();
-        $todaylectureCount = $student->lectures()->wherePivot('date' , Carbon::now()->format('Y-m-d'))->count();
-        $todayViewsSum = $student->lectures()->wherePivot('date' , Carbon::now()->format('Y-m-d'))->sum('views');
-        
-        return ResponseHelper::success(['lectureCount' => $todaylectureCount, 
-                                        'sumOfViews' => $todayViewsSum],
-                                 'count of lectures has been viewed today');
+        $student = Student::where('user_id', Auth::id())->first();
+        $todayAttencesRec = $student->lectures()->wherePivot('date', Carbon::now()->format('Y-m-d'));
+        if ($todayAttencesRec) {
+            $todaylectureCount = $todayAttencesRec->count();
+            $todayViewsSum = $todayAttencesRec->sum('views');
+            return ResponseHelper::success(
+                [
+                    'lectureCount' => $todaylectureCount,
+                    'sumOfViews' => $todayViewsSum
+                ],
+                'count of lectures has been viewed today'
+            );
+        } else
+            return ResponseHelper::success(
+                [
+                    'lectureCount' => 0,
+                    'sumOfViews' => 0
+                ],
+                'no lecture seen tody'
+            );
     }
 }
